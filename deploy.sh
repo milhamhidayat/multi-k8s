5e750a301316a6843da2e49a5320ea9f00b76b86
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-04-07 14:08:49 +0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-docker build -t photon628/multi-client -f ./client/Dockerfile ./client
-docker build -t photon628/multi-server -f ./server/Dockerfile ./server
-docker build -t photon628/multi-worker -f ./worker/Dockerfile ./worker
-docker push photon628/multi-client
-docker push photon628/multi-server
-docker push photon628/multi-worker
+docker build -t photon628/multi-client:latest -t photon628/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t photon628/multi-server:latest -t photon628/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t photon628/multi-worker:latest -t photon628/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push photon628/multi-client:latest
+docker push photon628/multi-server:latest
+docker push photon628/multi-worker:latest
+docker push photon628/multi-client:$SHA
+docker push photon628/multi-server:$SHA
+docker push photon628/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=photon628/multi-server
+kubectl set image deployments/server-deployment server=photon628/multi-server:$SHA
+kubectl set image deployments/client-deployment client=photon628/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=photon628/multi-worker:$SHA
